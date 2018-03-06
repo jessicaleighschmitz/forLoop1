30ebd30c954ff66bf65596f258a008fa6142303c
Guest User <Guest@Epicodus-10E.local>
2018-03-05 18:47:41 -0800

    add text to outpit div

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,19 @@
 $(document).ready(function() {
   $('#input form').submit(function(event) {
     event.preventDefault();
-
+    debugger;
     var bigNum = parseInt($('input#userInput').val());
     var multipleNum = parseInt($('input#countBy').val());
     var multiples = [];
 
-      for (var i = 0; i <= bigNum; i += multipleNum) {
-      multiples.push(i);
-      console.log('hi');
-     document.write(multiples);
+      for (var i = multipleNum; i <= bigNum; i += multipleNum) {
+
+        if (bigNum % i === 0) {
+          multiples.push(i);
+        };
+
+
+     $('#output').text(multiples);
   };
  });
 });
